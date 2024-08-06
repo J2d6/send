e77d5ed30fe5c965332950545dec34d7e6d5e0bf
J2d6 <j2d6.pro@gmail.com>
2024-08-06 09:16:22 +0300

    intial create - add dependencie clikt

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    kotlin("jvm") version "1.8.21"
+    application
+}
+
+group = "dev.j2d6"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+
+    implementation("com.github.ajalt.clikt:clikt:4.4.0")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(11)
+}
+
+application {
+    mainClass.set("MainKt")
+}
